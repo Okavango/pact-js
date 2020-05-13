158a5c272de6ffab4748b7ef564cbcc871259ec3
Ilya Lyalin <ilyalin@aligntech.com>
2020-05-13 11:07:16 +0300

    xml in request body

--- examples/v3/simple/simple.js
+++ examples/v3/simple/simple.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const { resolve } = require('path');
-const { MatchersV3, PactV3 } = require('@pact-foundation/pact/v3');
+const { MatchersV3, PactV3, XmlBuilder } = require('@pact-foundation/pact/v3');
 const chai = require("chai")
 
 const {
@@ -26,12 +26,47 @@ describe('Pact with IDS', () => {
   describe('getInfo', () => {
     beforeEach(() => {
       provider
+        .given("i have a list of projects")
         .uponReceiving("a request for projects")
         .withRequest({
           method: 'POST',
           path: '/path',
-          query: { from: "today" }
-          //headers: { Accept: "application/json" }
+          query: { from: "today" },
+          headers: { 'Content-Type': "application/xml" },
+          body: new XmlBuilder("1.0", "UTF-8", "ns1:projects").build(el => {
+            el.setAttributes({
+              id: "1234",
+              "xmlns:ns1": "http://some.namespace/and/more/stuff",
+            })
+            el.eachLike(
+              "ns1:project",
+              {
+                id: integer(1),
+                type: "activity",
+                name: string("Project 1"),
+                // TODO: implement XML generators
+                // due: timestamp(
+                //   "yyyy-MM-dd'T'HH:mm:ss.SZ",
+                //   "2016-02-11T09:46:56.023Z"
+                // ),
+              },
+              project => {
+                project.appendElement("ns1:tasks", {}, task => {
+                  task.eachLike(
+                    "ns1:task",
+                    {
+                      id: integer(1),
+                      name: string("Task 1"),
+                      done: boolean(true),
+                    },
+                    null,
+                    { examples: 5 }
+                  )
+                })
+              },
+              { examples: 2 }
+            )
+          })
         }).willRespondWith({
           status: 200,
           headers: { "Content-Type": "application/json" },
@@ -55,15 +90,33 @@ describe('Pact with IDS', () => {
     });
 
     it('request to IDS', async () => {
-      const result = provider.executeTest(
-        async (mockserver) => fetch( mockserver.url + '/path?from=today', {method: 'POST'})
-      );
-      try {
-        const res = await result;
-        expect(res.status).to.be.equal(200);
-      } catch(e) {
-        console.log('Error:', e)
-      }
+        let response;
+        //try {
+          const result = provider.executeTest(
+            async (mockserver) => fetch( mockserver.url + '/path?from=today', {
+              method: 'POST',
+              headers: { "Content-Type": "application/xml" },
+              body: `<?xml version="1.0" encoding="UTF-8"?>
+                <projects foo="bar">
+                  <project id="1" name="Project 1" due="2016-02-11T09:46:56.023Z">
+                    <tasks>
+                      <task id="1" name="Do the laundry" done="true"/>
+                      <task id="2" name="Do the dishes" done="false"/>
+                      <task id="3" name="Do the backyard" done="false"/>
+                      <task id="4" name="Do nothing" done="false"/>
+                    </tasks>
+                  </project>
+                  <project/>
+                </projects>
+              `
+            })
+          );
+          response = await result;
+        // } catch (e) {
+        //   console.error(e);
+        // }
+        expect(response).to.not.be.undefined;
+        expect(response.status).to.be.equal(200);
     });
   });
 
